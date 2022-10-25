f03ca7a393ea05210130421d6782c69ce5c5bc56
Faith M. Roberts <faithroberts708@gmail.com>
2022-10-25 14:29:04 +0100

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+git 
 
 function App() {
   return (
